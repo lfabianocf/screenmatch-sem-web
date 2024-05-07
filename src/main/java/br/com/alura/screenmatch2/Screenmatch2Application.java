4a021aa88019bd2b29ac45ef9673e03063e41d38
Fabiano Fernandes <lfabianocf@gmail.com>
2024-05-07 11:41:39 -0300

    Inicio sreenmatch com spring sem web.

---
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+}
