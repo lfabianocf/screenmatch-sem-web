fb3873e9dd216b457c650212a78e436b2fda0171
Fabiano Fernandes <lfabianocf@gmail.com>
2024-05-07 12:29:17 -0300

    pacote-service-class-consumoapi-teste-apikey

--- src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch2;
 
+import br.com.alura.screenmatch2.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,17 @@ public class Screenmatch2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("teste console java");
+
+        var consumoApi = new ConsumoApi();
+
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&season=1&apikey=88d8c2ff");
+
+		System.out.println(json);
+
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+
+		System.out.println(json);
+
+
 	}
 }
