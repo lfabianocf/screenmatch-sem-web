187513c64fcdacd7905ca097c5bfedac95bdc0c7
Fabiano Fernandes <lfabianocf@gmail.com>
2024-05-07 11:44:03 -0300

    Implementando commandlineruuner para console java.

--- src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -1,13 +1,18 @@
 package br.com.alura.screenmatch2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Screenmatch2Application {
+public class Screenmatch2Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Screenmatch2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("teste console java");
+	}
 }
