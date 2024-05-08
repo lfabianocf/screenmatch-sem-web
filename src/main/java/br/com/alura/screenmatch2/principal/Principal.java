60d0bb8aa97f60f6cf1c5fc338c4bee45414f288
Fabiano Fernandes <lfabianocf@gmail.com>
2024-05-07 21:55:15 -0300

    filtrando-episodio-por-data-lacamento

--- src/main/java/br/com/alura/screenmatch2/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch2/principal/Principal.java
@@ -7,6 +7,8 @@ import br.com.alura.screenmatch2.model.Episodio;
 import br.com.alura.screenmatch2.service.ConsumoApi;
 import br.com.alura.screenmatch2.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -83,6 +85,30 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
 
+        LocalDate dataBusca = LocalDate.of(ano, 1,1);
+
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
+
+        /*episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        String.format(String.format("Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formatador)))
+                ));*/
     }
 }
